f21a0b093b84d5e9bca486818e7ba104288d5ba3
enes <enes.karakas@student.kdg.be>
2018-02-12 12:23:35 +0100

    Initial commit

---
+++ src/main/java/be/kdg/fraudedetection/FraudeDetectionApplication.java
@@ -0,0 +1,12 @@
+package be.kdg.fraudedetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudeDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudeDetectionApplication.class, args);
+	}
+}
